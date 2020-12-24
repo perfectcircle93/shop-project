dfb37e60e768c03082696a6c871205773d4125e7
Magdalena Świderska <m.swiderska@outlook.com>
2020-12-24 01:35:17 +0100

    Add RWD into header component

--- src/components/layout/Header/Header.js
+++ src/components/layout/Header/Header.js
@@ -21,15 +21,15 @@ const Component = ({ className, cartItems }) => (
       alignItems="center"
       className={styles.container}
     >
-      <Grid item xs>
+      <Grid item xs={9} md>
         <PageNav></PageNav>
       </Grid>
-      <Grid item container justify="center" xs={6}>
+      <Grid item container justify="center" xs={12} md={6}>
         <Link variant="h6" component={RouterLink} to="/" underline="none">
           Food Travelers
         </Link>
       </Grid>
-      <Grid item container justify="flex-end" xs alignItems="flex-end">
+      <Grid item container justify="flex-end" xs={3} alignItems="flex-end">
         <Button>Cart({cartItems})</Button>
       </Grid>
     </Grid>
